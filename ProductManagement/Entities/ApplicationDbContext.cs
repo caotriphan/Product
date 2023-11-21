c5c1867b8d9d9cbdd273d945a81ba8ad378c836d
caotriphan <phancaotri2212@gmail.com>
2023-11-21 21:43:36 +0700

    set up project

---
+++ ProductManagement/Entities/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Entities
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ProductModel> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=.\\SQLExpress;database=ProductEntity;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ ProductManagement/Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using ProductManagement.Entities;
+using ProductManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagement.Services
+{
+    interface IProductService
+    {
+        Task<List<ProductModel>> GetProductsAsync();
+        Task<ProductModel> GetProductAsync(int id);
+        Task<ProductModel> AddProductAsync();
+        Task UpdateProductAsync(ProductModel product);
+        Task DeleteProductAsync(int id);
+    }
+
+    public class ProductService : IProductService
+    {
+        private ApplicationDbContext db = new();
+
+        public Task<ProductModel> AddProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductModel> GetProductAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductModel>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductAsync(ProductModel product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
